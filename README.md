9e6b1cf8b5a5f4e3f8005c71c5c8272b07cb5de1
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-02 10:15:52 -0600

    giff: creando módulo shared y componente sidebar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GifsApp
+
+#### Generar componenete sin archivo de test y archivo de estilos
+
+ng g c nombre_carpeta/nombre_componente --skip-tests -is
+
+* is (inline style)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
