ad41311473a0c7963850af9646fb7716a867cacd
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-04 12:45:10 -0600

    giff: local-storage - persistiendo información en memoria del navegador

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -17,7 +17,12 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    // if(localStorage.getItem('historial') ){
+    //     this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+  }
 
   buscarGifs(query: string = '') {
     //Asegurar que el dato sea registrado con minusculas
@@ -28,6 +33,8 @@ export class GifsService {
       this._historial.unshift(query);
       // Crece el arreglo solomanete si se agrea un nuevo elemento al mismo
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ));
     }
     this.http
       .get<SearchGifsResponse>(
